77b4227e89b24cd853c87ce4ad136ce6ad9a1fb9
includeMaple <maple_dby@163.com>
2021-04-07 22:08:27 +0800

    add response

--- mi/mi/src/main.js
+++ mi/mi/src/main.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import router from './router'
+import axios from 'axios'
+// 说是这个插件主要是为了能直接使用this，也就是把axios挂载到Vue实例
+import VueAxios from 'vue-axios'
 
-Vue.use(VueRouter)
+import App from './App.vue'
 
+// Vue.use(VueRouter)
+
+// 根据前端的跨域方式做跳转
+axios.defaults.baseURL = '/api';
+// 设置超时时间为8秒
+axios.defaults.timeout = 8000;
+// 错误拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data
+  if (res.status === 0) {
+    // 0表示成功
+    return res.data
+  } else if (res.status === 10) {
+    // 10表示未登录,跳转到登录页面
+    window.location.href = '/#/login';
+  } else {
+    // 其他报错，直接弹出
+    alert(res.msg);
+  }
+})
+Vue.use(VueAxios, axios)
+// 生产环境提示，默认是false，如果开启，一些vue的底层代码报错等就会出现
 Vue.config.productionTip = false
 
 new Vue({
