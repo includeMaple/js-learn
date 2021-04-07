3db6e345751aab4d7b2f682080951998dbe0ebf5
includeMaple <maple_dby@163.com>
2021-04-07 22:23:28 +0800

    edit router

--- mi/mi/src/main.js
+++ mi/mi/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import router from './router'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
